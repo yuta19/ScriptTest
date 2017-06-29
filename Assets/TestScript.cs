7a4c06b526d8b4555e99ed90dae6080bff2231ec
野口湧太 <yui.queen19@gmail.com>
2017-06-29 21:51:23 +0900

    ScriptTestプロジェクト　一回目のコミット
    Lesson4の課題です。

---
+++ Assets/TestScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+
+	public class Boss{
+		private int hp =100;
+		public int mp =53;
+		private int power =25;
+
+		public void Attack(){
+			Debug.Log (this.power + "のダメージを与えた");
+		}
+
+		public void Defence(int damage){
+			Debug.Log (damage + "のダメージを受けた");
+			this.hp -= damage;
+		}
+
+		public void Magic(){
+
+			for(int i = 0; i < 10; i++){
+				mp -= 5;
+			}
+
+
+			if (mp < 5) {
+				Debug.Log ("MPが足りないため魔法が使えない。" );
+			} else {
+				Debug.Log ("魔法攻撃をした。残りMPは" + this.mp);
+			}
+		}
+
+
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		int[] array = new int[5];
+
+		array [0] = 10;
+		array [1] = 20;
+		array [2] = 30;
+		array [3] = 40;
+		array [4] = 50;
+
+		for (int i = 4; i > -1; i--) {
+			Debug.Log (array [i]);
+		}
+	  
+
+		Boss lastboss = new Boss ();
+
+		lastboss.Attack ();
+		lastboss.Defence (3);
+		lastboss.Magic ();
+
+
+			
+
+
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/TestScript2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (i);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
